63514cbe68eda1f418eae97252ccf862613163e3
Juan Ahumada <juan.ahumada94@gmail.com>
2021-05-05 07:40:34 -0500

    MySQL

--- mysql/main.py
+++ mysql/main.py
@@ -10,7 +10,7 @@ database = mysql.connector.connect(
 )
 
 # Validar conexion
-print(database)
+#print(database)
 
 # Cursor
 cursor = database.cursor(buffered=True)
@@ -36,7 +36,53 @@ CREATE TABLE IF NOT EXISTS vehiculos(
   )
 """)
 
+"""
 cursor.execute("SHOW TABLES")
 
 for table in cursor:
   print (table)
+"""
+
+# Insert
+#cursor.execute("INSERT INTO vehiculos VALUES(null, 'Honda', 'Accord', 30000)")
+"""
+autos = [
+  ('Honda', 'Civic', 28000),
+  ('Audi', 'A4', 60000),
+  ('BMW','X7', 50000),
+  ('Kia','Sorento', 40000)
+]
+
+cursor.executemany("INSERT INTO vehiculos VALUES(null,%s,%s,%s)", autos)
+
+database.commit();
+"""
+
+# Listar/ Mostrar
+
+cursor.execute("SELECT * FROM vehiculos")
+
+resultado = cursor.fetchall()
+
+print("--- Todos los autos ---")
+for auto in resultado:
+  print(auto[0],auto[1],auto[2],auto[3])
+
+"""
+cursor.execute("SELECT * FROM vehiculos")
+auto = cursor.fetchone()
+print(auto)
+"""
+
+# Borrar / DELETE
+"""
+cursor.execute("DELETE FROM vehiculos")
+database.commit()
+"""
+
+# Actualizar / Update
+cursor.execute("UPDATE vehiculos SET modelo='a7 sport' WHERE marca = 'Audi'")
+database.commit()
+
+print("---- Actualizacion de Datos ----")
+print(cursor.rowcount, "Dato actualizado")
